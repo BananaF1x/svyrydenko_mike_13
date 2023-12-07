7ec88fa5696497e1d49d1537d47b5791ea826b17
BananaF1x <brovkoman@gmail.com>
2023-12-07 11:47:42 +0200

    Додав скрипт для oriented

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// burger menu
+// burger menu header
 const headerMenu = document.querySelector(".header-menu");
 document.querySelector(".menu-btn").addEventListener("click", function(){
     headerMenu.classList.add("active");
@@ -7,8 +7,8 @@ document.querySelector(".menu__exit-btn").addEventListener("click", function(){
     headerMenu.classList.remove("active");
 });
 
-// slayder
-const teamTabs = document.querySelectorAll("div.team__tabs, span.team__list__point");
+//Tabs team
+const teamTabs = document.querySelectorAll("div.team__tabs, button.team__list__point");
 teamTabs.forEach((e)=>{
     if(e.classList.contains("team__list__point")) {
         e.addEventListener("click", function() {
@@ -20,6 +20,24 @@ teamTabs.forEach((e)=>{
             if(e.id == "1") document.querySelectorAll(".team__tabs")[0].classList.add("active-tab");
             if(e.id == "2") document.querySelectorAll(".team__tabs")[1].classList.add("active-tab");
             if(e.id == "3") document.querySelectorAll(".team__tabs")[2].classList.add("active-tab");
+        });
+    };
+});
+
+//Tabs oriented
+const orientedTabs = document.querySelectorAll("div.oriented__container, button.oriented__btn");
+orientedTabs.forEach((e)=>{
+    if(e.classList.contains("oriented__btn")) {
+        e.addEventListener("click", function() {
+            orientedTabs.forEach((item) => {
+                if(item.classList.contains("active-btn")) item.classList.remove("active-btn");
+                if(item.classList.contains("active-container")) item.classList.remove("active-container");
+            });
+            e.classList.add("active-btn");
+            if(e.id == "11") document.querySelectorAll(".oriented__container")[0].classList.add("active-container");
+            if(e.id == "22") document.querySelectorAll(".oriented__container")[1].classList.add("active-container");
+            if(e.id == "33") document.querySelectorAll(".oriented__container")[2].classList.add("active-container");
+            if(e.id == "44") document.querySelectorAll(".oriented__container")[3].classList.add("active-container");
         });
     };
 });
