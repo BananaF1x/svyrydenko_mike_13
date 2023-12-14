6c2cf9fbb7f2d9562ff224fdecce2d7cf053ce0d
BananaF1x <brovkoman@gmail.com>
2023-12-14 10:29:20 +0200

    Виправив баг з меню

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,13 @@ const headerMenu = document.querySelector(".header-menu");
 document.querySelector(".menu-btn").addEventListener("click", function(){
     headerMenu.classList.add("active");
 });
+
 document.querySelector(".menu__exit-btn").addEventListener("click", function(){
     headerMenu.classList.remove("active");
+});
+
+window.addEventListener("resize", function(){
+    if(headerMenu.offsetWidth >= 768) {
+        headerMenu.classList.remove("active");
+    };
 });
